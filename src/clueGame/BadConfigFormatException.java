507d87b1f606792fde2b4971dc2d93bed7061080
sonomedicus <jsnell02@gmail.com>
2015-02-20 16:26:48 -0700

    FormatExcpetion class change
    Changed it to accept a string message

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -5,28 +5,29 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class BadConfigFormatException extends Exception {
-	int columns;
-	Character room_initial;
+	//int columns;
+	String message;
 	public BadConfigFormatException () {}
     
-	public BadConfigFormatException (int columns) throws Exception{
+	/*public BadConfigFormatException (int columns) throws Exception{
 	super();
 	this.columns = columns;
 	 File file = new File("logfile.txt");
 	 FileWriter file_write = new FileWriter(file);
 	 file_write.write( toString());
 	 file_write.close();
-	}
-	public BadConfigFormatException (Character room_initial) throws Exception{
+	}*/
+	public BadConfigFormatException (String room_initial) throws Exception{
 		super();
-		this.room_initial = room_initial;
+		this.message = room_initial;
 		File file = new File("logfile.txt");
 		FileWriter file_write = new FileWriter(file);
-		file_write.write("Room initial " + room_initial + " is wrong");
+		file_write.write(toString());
+		file_write.close();
 	}
 	@Override
 	public String toString() {
-		return "BadConfigFormatException [columns=" + columns + "]";
+		return "Error " + message + " occured";
 	}
 	
 	
