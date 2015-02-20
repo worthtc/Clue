5f3a09b52cc904f40a7033f2da501fa5446e4f8a
sonomedicus <jsnell02@gmail.com>
2015-02-20 14:07:19 -0700

    Added in more functionality to the Excpetion class i wrote
    Added in a throws clause for chars

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 public class BadConfigFormatException extends Exception {
 	int columns;
+	Character room_initial;
 	public BadConfigFormatException () {}
     
 	public BadConfigFormatException (int columns) throws Exception{
@@ -16,7 +17,13 @@ public class BadConfigFormatException extends Exception {
 	 file_write.write( toString());
 	 file_write.close();
 	}
-
+	public BadConfigFormatException (Character room_initial) throws Exception{
+		super();
+		this.room_initial = room_initial;
+		File file = new File("logfile.txt");
+		FileWriter file_write = new FileWriter(file);
+		file_write.write("Room initial " + room_initial + " is wrong");
+	}
 	@Override
 	public String toString() {
 		return "BadConfigFormatException [columns=" + columns + "]";
